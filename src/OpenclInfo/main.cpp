3675ab09be4398534c6877244a1821e8abfb7701
cpapideveloper <cpapideveloper@gmail.com>
2024-09-28 22:24:33 -0700

    Add OpenCL hpp headers and OpenClInfo project

---
+++ src/OpenclInfo/main.cpp
@@ -0,0 +1,48 @@
+#include <CL/opencl.hpp>
+#include <iostream>
+
+int main()
+{
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    if (platforms.empty())
+    {
+        std::cerr << "No platforms found!" << std::endl;
+        return -1;
+    }
+
+    auto platform = platforms.front();
+    std::vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+    if (devices.empty()){
+        std::cerr << "No devices found!" << std::endl;
+        return -1;
+    }
+
+    for(auto device : devices)
+    {
+        auto name = device.getInfo<CL_DEVICE_NAME>();
+        auto vendor = device.getInfo<CL_DEVICE_VENDOR>();
+        auto version = device.getInfo<CL_DEVICE_VERSION>();
+        auto workItems = device.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>();
+        auto workGroups = device.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>();
+        auto computeUnits = device.getInfo<CL_DEVICE_MAX_COMPUTE_UNITS>();
+        auto globalMemory = device.getInfo<CL_DEVICE_GLOBAL_MEM_SIZE>();
+        auto localMemory = device.getInfo<CL_DEVICE_LOCAL_MEM_SIZE>();
+
+        std::cout << "OpenCL Device Info:"
+        << "\nName: " << name
+        << "\nVendor: " << vendor
+        << "\nVersion: " << version
+        << "\nMax size of work-items: (" << workItems[0] << "," << workItems[1] << "," << workItems[2] << ")"
+        << "\nMax size of work-groups: " << workGroups
+        << "\nNumber of compute units: " << computeUnits
+        << "\nGlobal memory size (bytes): " << globalMemory
+        << "\nLocal memory size per compute unit (bytes): " << localMemory/computeUnits
+        << std::endl;
+    }
+
+    return 0;
+}
